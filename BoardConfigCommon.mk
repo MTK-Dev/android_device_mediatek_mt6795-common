8cd106bd82f472fbb8f8c5182f975092bc41889f
bemolxd <kalkobb99@gmail.com>
2016-12-28 12:32:27 +0100

    mt6795: Add base files

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,58 @@
+# mt6795 platform board config
+LOCAL_PATH := device/mediatek/mt6795-common
+include vendor/mtk/config/board.mk
+
+# Platform
+ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_BOARD_PLATFORM := mt6795
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_FACTORYIMAGE := true
+
+# CPU
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
+
+TARGET_CPU_CORTEX_A53 := true
+
+TARGET_BOOTLOADER_BOARD_NAME := mt6795
+
+# Common properties
+TARGET_SYSTEM_PROP := $(COMMON_PATH)/system.prop
+
+# Kernel
+TARGET_USES_64_BIT_BINDER := true
+TARGET_IS_64_BIT := true
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --pagesize 2048 --kernel_offset 0x00008000 --ramdisk_offset 0x03f88000 --second_offset 0x00e88000 --tags_offset 0x0df88000 --board Bemolx
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+MTK_APPENDED_DTB_SUPPORT := yes
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/mediatek/mt6795-common/ril
+
+# CMHW
+BOARD_USES_CYANOGEN_HARDWARE := true
+BOARD_HARDWARE_CLASS += $(COMMON_PATH)/cmhw
+
+# Seccomp filter
+BOARD_SECCOMP_POLICY := $(LOCAL_PATH)/seccomp
+
+# Misc
+EXTENDED_FONT_FOOTPRINT := true
